2953c77f5d0b3e66deb7d200379b0093eb2eb037
Jerry Lin <jerry.jzlin@gmail.com>
2017-10-01 13:36:01 +0800

    任務 10：進階練習 自訂輸入屬性名稱 ( 讓元件內的屬性名稱與在範本使用的屬性名稱不一樣 )

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  @Input() todos = [];
+  @Input('data') todos = [];
 
   constructor() { }
 
