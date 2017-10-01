8bf86505adb2df329c40255c1981b81ec05cffab
Jerry Lin <jerry.jzlin@gmail.com>
2017-10-01 13:34:38 +0800

    任務 10：套用 @Input() 裝飾器 (Decorator)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TitleComponent
+    TitleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
